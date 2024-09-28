9b164348fed1b5ccdf2317abc9cd307c0da138ba
Alexander Gribkov <gribkovalexander@gmail.com>
2024-09-28 20:45:30 +0300

    create test

---
+++ practice/practice3/src/test/kotlin/ru/mirea/testingandverificationsoftware/practice/pracice3/AudioMixerTest.kt
@@ -0,0 +1,44 @@
+package ru.mirea.testingandverificationsoftware.practice.pracice3
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class AudioMixerTest {
+
+    @Test
+    fun `test mix clips`() {
+        val audioMixer = AudioMixer()
+        val clips = getTestAudioClips()
+        val outputFile = getOutputFile()
+
+        audioMixer.mixAudioClips(clips, outputFile)
+
+        assertOutputFile(outputFile)
+    }
+
+    private fun getTestAudioClips(): List<Clip> =
+        listOf(
+            Clip(File("src/test/resources/audio/1.wav"), 1.0, 5.0),
+            Clip(File("src/test/resources/audio/2.wav"), 1.0, 5.0),
+            Clip(File("src/test/resources/audio/3.wav"), 1.0, 5.0),
+            Clip(File("src/test/resources/audio/4.wav"), 1.0, 5.0),
+        )
+
+    private fun getOutputFile(): File =
+        File("src/test/resources/audio/output_mix.wav").apply {
+            if (exists()) {
+                delete()
+                createNewFile()
+            }
+        }
+
+    private fun assertOutputFile(outputFile: File) {
+        assertTrue(outputFile.exists())
+        assertTrue(outputFile.isFile)
+
+        val expected = File("src/test/resources/audio/expected.wav")
+        assertEquals(expected.readText(), outputFile.readText())
+    }
+}
