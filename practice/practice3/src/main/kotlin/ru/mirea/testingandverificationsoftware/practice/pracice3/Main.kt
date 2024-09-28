cd0c24b954b7814c29f5b1b255968471460207db
Alexander Gribkov <gribkovalexander@gmail.com>
2024-09-28 19:14:29 +0300

    dev finished

--- practice/practice3/src/main/kotlin/ru/mirea/testingandverificationsoftware/practice/pracice3/Main.kt
+++ practice/practice3/src/main/kotlin/ru/mirea/testingandverificationsoftware/practice/pracice3/Main.kt
@@ -1,5 +1,40 @@
 package ru.mirea.testingandverificationsoftware.practice.pracice3
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val audioMixer = AudioMixer()
+    val clips = getAudioClips()
+    val outputFile = File("assets/audio/output_mix.wav")
+    if (outputFile.exists()) {
+        outputFile.delete()
+        outputFile.createNewFile()
+    }
+
+    try {
+        audioMixer.mixAudioClips(clips, outputFile)
+        println("Микс создан: ${outputFile.absolutePath}")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
+
+private fun getAudioClips(): List<Clip> {
+    val scanner = java.util.Scanner(System.`in`)
+    println("Введите количество аудиофайлов для микширования:")
+    val fileCount = scanner.nextInt()
+
+    val clips = mutableListOf<Clip>()
+    for (i in 1..fileCount) {
+        println("Введите имя файла #$i:")
+        val fileName = scanner.next()
+        println("Введите начало отрезка в секундах для файла #$i:")
+        val startTime = scanner.nextDouble()
+        println("Введите конец отрезка в секундах для файла #$i:")
+        val endTime = scanner.nextDouble()
+
+        clips.add(Clip(File("assets/audio/$fileName.wav"), startTime, endTime))
+    }
+    return clips
+}
+
